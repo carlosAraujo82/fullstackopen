b2d3d82d6d3abe234327271db25d3ff0c28c6ae7
PC-CARLOS\Carlos <cmsfaraujo@gmail.com>
2021-09-22 21:32:53 +0100

    Exercise 2.1

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,71 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
 
-  let randomNumber = 0
-
-  const getRandomAnecdote = () => {
-    randomNumber = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randomNumber);
-  }
-
-  const vote = () => {
-    const copy = [...votes];
-    copy[selected] += 1;     
-    setVotes(copy);
-  }
-
-  const MostVotedAnecdote = () =>
-  {
-    let index = GetIndexOfMostVoted(votes);
-    if (index === -1)
-      return (<p>...</p>);
-    else
-    {
-      let numberOfVotes = votes[index];
-      return(<div><p>{anecdotes[index]}</p><p>has {numberOfVotes} votes</p></div>);
-    }  
-  }
-
-  function GetIndexOfMostVoted(arrayOfVotes)
-  {
-    var maxValue = arrayOfVotes[0];
-    var maxIndex = 0;
-    for (let i = 1; i < arrayOfVotes.length; i++)
-    {
-      if (arrayOfVotes[i] > maxValue)
-      {
-        maxValue = arrayOfVotes[i];
-        maxIndex = i;
-      }
-    }
-
-    if (maxValue === 0)
-      return -1;
-    else
-      return maxIndex;
-  }
-
+const App = ({ notes }) => {
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes.</p>
-      <p><button onClick={vote}>vote</button><button onClick={getRandomAnecdote}>Next Anecdote</button></p>
-      <h1>Anecdote with most votes</h1>
-      <MostVotedAnecdote/>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
